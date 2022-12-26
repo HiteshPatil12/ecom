5dfc1780240deb01a5045257085db158d7c7cd54
Hitesh <hiteshpatil394@gmail.com>
2022-12-26 07:35:00 +0000

    updated

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+  productList;
+  priceList;
+  constructor(){
+    this.productList = ['Shoe', 'Shirt', 'Phone'];
+    this.priceList = ['9000', '1300', '70000'];
+  }
 
+  addToCart(input){
+    console.log(input);
+  }
 }
